ae829fe66e98bc8391b0d5f605ff240754620357
Aizirek <aizirek.abdubaetova@gmail.com>
2022-08-03 16:07:15 +0200

    filter menu branch

---
+++ src/test/java/com/navfort/pages/FilterMenu_Page.java
@@ -0,0 +1,40 @@
+package com.navfort.pages;
+
+import com.navfort.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FilterMenu_Page {
+    public FilterMenu_Page(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//span[@class='title title-level-1']")
+    public WebElement fleetMenuBtn;
+
+    @FindBy(xpath = "//span[@class='title title-level-2']")
+    public WebElement VehicleBtn;
+
+
+    @FindBy(xpath = "//i[@class='fa-filter hide-text']")
+    public WebElement filterMenuBtn;
+
+    @FindBy(xpath = "//a[@class='action btn mode-icon-only']")
+    public WebElement filterIcon;
+
+
+    @FindBy(xpath = "//button[@class='ui-multiselect ui-corner-all select-filter-widget']")
+    public WebElement manageFilterBtn;
+
+    @FindBy(id = "ui-multiselect-0-0-option-0")
+    public WebElement licencePlate;
+
+    @FindBy(id = "ui-multiselect-0-0-option-3")
+    public WebElement location;
+
+
+    @FindBy(xpath = "//input[@type='search']")
+    public WebElement filterInput;
+
+}
---
+++ src/test/resources/features/FilterMenu.feature
@@ -0,0 +1,35 @@
+
+  Feature: Filter Menu
+  User Story:
+  As a user, I should be able to use FILTER option under Fleet-Vehicle page
+
+  Background: For the scenarios in this feature file, the user is expected to be logged in as a Driver,
+  Store Manager or Sales Manager
+    Given The user is on the Fleet Vehicles page
+
+    #passed
+   Scenario: Manage Filter button visibility
+    When user clicks on filter icon
+    Then Manage Filter button should be visible
+
+    #passed
+   Scenario: Users can apply filters from 'Manage Filters' menu
+     When user clicks on filter button
+     And user clicks on Manage Filters menu
+     Then user can apply any of the filters
+
+      #passed
+    Scenario: 3- User can apply filters by typing the filter name, from 'Manage Filters' menu.
+      When the user clicks on filter menu icon
+      And the user clicks on manage filters icon
+      And user types a filter name
+      Then user should be able to select that filter
+
+
+
+
+
+
+
+
+
